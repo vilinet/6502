5bb419639859acab3e8dd37f507778e6cd9f69a0
vilmos <vilinet9@gmail.com>
2019-09-07 16:20:31 +0100

    added new classes

---
+++ nes/Ppu.cs
@@ -0,0 +1,87 @@
+﻿using emulator6502;
+
+namespace NES
+{
+
+    public class Ppu : IAddressable
+    {
+        public ushort From { get; } = 0x2000;
+        public ushort To { get; } = 0x2007;
+        
+        public bool FrameComplete { get; private  set; }
+        private short Cycle { get; set; }
+        private short Scanline { get; set; }
+
+        public PPURegisters PPURegisters { get; set; } = new PPURegisters();
+   
+
+        public void Clock()
+        {
+            Cycle++;
+            if (Cycle >= 341)
+            {
+                Cycle = 0;
+                Scanline++;
+                if (Scanline >= 261)
+                {
+                    Scanline = -1;
+                    FrameComplete = true;
+                }
+            }
+        }
+
+        public void Write(ushort address, byte value)
+        {
+            
+        }
+
+        public byte Read(ushort address)
+        {
+            var result =  (address-0x2000) % 8;
+            switch (result)
+            {
+                case 0:
+                    return (byte)PPURegisters.PPUCTRL;
+                case 1:
+                    return (byte)PPURegisters.PPUMASK;
+                case 2:
+                    return (byte)PPURegisters.PPUSTATUS;
+                case 3:
+                    return PPURegisters.OEMADDR;
+                case 4:
+                    return PPURegisters.OAMDATA;
+                case 5:
+                    return PPURegisters.PPUSCROLL;
+                case 6:
+                    return PPURegisters.PPUADDR;
+                case 7:
+                    return PPURegisters.PPUDATA;
+            }
+
+            return 0;
+        }
+
+        public void Reset()
+        {
+            PPURegisters.PPUCTRL = 0;
+            PPURegisters.PPUMASK = (PPUMASK)((byte)PPURegisters.PPUMASK | 0b1000000);
+            PPURegisters.PPUSTATUS = (PPUSTATUS)(0b10111111 & (byte)PPURegisters.PPUSTATUS);
+            PPURegisters.OEMADDR = 0;
+            PPURegisters.OAMDATA = 0;
+            PPURegisters.PPUSCROLL = 0;
+            PPURegisters.PPUADDR = 0;
+        }
+
+        public void PowerOn()
+        {
+            PPURegisters.PPUCTRL = 0;
+            PPURegisters.PPUMASK = 0;
+            PPURegisters.PPUSTATUS = (PPUSTATUS)(0b10111111 & (byte)PPURegisters.PPUSTATUS);
+            PPURegisters.OEMADDR = 0;
+            PPURegisters.OAMDATA = 0;
+            PPURegisters.PPUSCROLL = 0;
+            PPURegisters.PPUADDR = 0;
+        }
+      
+    }
+}
---
+++ nes/PpuRegisters.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NES
+{
+    [Flags]
+    public enum PPUCTRL : byte
+    {
+        Zero = 0,
+        /// <summary>
+        /// Name Table address bytes
+        /// (0 = $2000; 1 = $2400; 2 = $2800; 3 = $2C00)
+        /// N0 Low
+        /// N1 High
+        /// </summary>
+        N0, N2,
+        /// <summary>
+        /// VRAM address increment per CPU read/write of PPUDATA
+        /// </summary>
+        I,
+        /// <summary>
+        /// Sprite pattern table address for 8x8 sprites
+        /// (0: $0000; 1: $1000; ignored in 8x16 mode)
+        /// </summary>
+        S,
+
+        /// <summary>
+        /// Sprite size (0: 8x8 pixels; 1: 8x16 pixels)
+        /// </summary>
+        H,
+        /// <summary>
+        /// PPU master/slave select
+        /// (0: read backdrop from EXT pins; 1: output color on EXT pins)
+        /// </summary>
+        P,
+        /// <summary>
+        /// Generate an NMI at the start of the
+        /// vertical blanking interval (0: off; 1: on)
+        /// </summary>
+        V
+    }
+
+    [Flags]
+    public enum PPUMASK : byte
+    {
+        Zero = 0,
+        /// <summary>
+        /// Grayscale
+        /// </summary>
+        g,
+        /// <summary>
+        /// Show background in leftmost 8 pixels of screen, 0: Hide
+        /// </summary>
+        m,
+        /// <summary>
+        ///  Show sprites in leftmost 8 pixels of screen, 0: Hide
+        /// </summary>
+        M,
+        /// <summary>
+        ///  Show background
+        /// </summary>
+        b,
+        s,
+        /// <summary>
+        ///  Emphasize red
+        /// </summary>
+        R,
+        /// <summary>
+        ///  Emphasize green
+        /// </summary>
+        G,
+        /// <summary>
+        ///  Emphasize blue
+        /// </summary>
+        B
+    }
+
+    [Flags]
+    public enum PPUSTATUS : byte
+    {
+        Zero = 0,
+        /// <summary>
+        /// sprite overflow
+        /// </summary>
+        O = 5,
+        /// <summary>
+        /// sprite 0 hit 
+        /// </summary>
+        S = 61,
+        /// <summary>
+        /// VBLANK
+        /// </summary>
+        V = 7
+    }
+
+    /// <summary>
+    /// All properties are in the correct order 0-7
+    /// </summary>
+    public class PPURegisters
+    {
+        public PPUCTRL PPUCTRL { get; set; } = new PPUCTRL();
+        public PPUMASK PPUMASK { get; set; } = new PPUMASK();
+        public PPUSTATUS PPUSTATUS { get; set; } = new PPUSTATUS();
+        public byte OEMADDR { get; set; }
+        public byte OAMDATA { get; set; }
+        public byte PPUSCROLL { get; set; }
+        public byte PPUADDR { get; set; }
+        public byte PPUDATA { get; set; }
+    }
+}
