ae37ba036a136fe13f5bec23ec45062fde12e4fe
vilinet9@gmail.com <Valaki899>
2019-09-20 21:35:27 +0200

    Changes..

---
+++ NES.Interfaces/IDisplay.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace NES.Interfaces
+{
+    public interface IDisplay
+    {
+        void DrawPixel(int x, int y, uint color);
+        
+        void FrameFinished();
+    }
+}
